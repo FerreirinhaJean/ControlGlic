cd022faee4de459e1d1590433966d0727fc8ad62
jean.ferreira <jean.ferreira@compasso.com.br>
2023-08-26 13:20:10 -0300

    Initial commit

---
+++ src/main/java/br/com/ferreira/ControlGlic/ControlGlicApplication.java
@@ -0,0 +1,13 @@
+package br.com.ferreira.ControlGlic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlGlicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlGlicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ferreira/ControlGlic/ControlGlicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ferreira.ControlGlic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlGlicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
